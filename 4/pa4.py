5d29ba32156fc8d9d9a65b3769dbe2f38ea990eb
greenmoon55 <greenmoon55@gmail.com>
2015-06-02 22:10:05 +0800

    Send all blocks and start from 2 for the last block because guess 1 will cancel out fake pad 1 and the original pad will always be a good padding

--- 4/pa4.py
+++ 4/pa4.py
@@ -3,12 +3,8 @@ from time import sleep
 
 TARGET = 'http://crypto-class.appspot.com/po?er='
 
-
 class PaddingOracle(object):
     def __init__(self, ct):
-        import pdb; pdb.set_trace()
-        self.iv = self.hex_to_byte(ct[:32])
-        ct = ct[32:]
         self.ct = []
         for i in xrange(0, len(ct), 32):
             print i
@@ -47,22 +43,25 @@ class PaddingOracle(object):
             return False  # bad padding
 
     def _query_block(self, test_block, index):
-        encoded_query = (test_block + self.ct[index]).encode('hex')
+        q = ''
+        for i in range(index):
+            q += self.ct[i]
+        encoded_query = (q + test_block + self.ct[index + 1]).encode('hex')
+        print encoded_query
         return self.query(encoded_query)
 
     def _get_pad(self, num):
         return str(bytearray([num] * num))
 
     def _generate_block(self, guess_byte, pad_num, block):
-        #import pdb; pdb.set_trace()
         padding = self._get_pad(pad_num)
         xor_val = self.strxor(guess_byte + b'\0' * (pad_num - 1), padding)
         print self.strxor(block, xor_val).encode('hex')
         return self.strxor(block, xor_val)
 
     def attack(self):
-        for i in range(len(self.ct)):
-            block = self.iv
+        for i in range(len(self.ct) - 2):
+            block = self.ct[i]
             pt_block = ''
             for j in range(1, 17):
                 for guess in """ etoanihsrdluwmycgf,bp.kv'"I-T;_HAWMSB!j?ExLCDzPJNq:YOGRFU1XK*V()Q/023548697][$@Z#%&+<=>\^`{|}~""":
@@ -72,10 +71,30 @@ class PaddingOracle(object):
                         block = ''.join(s)
                         pt_block = guess + pt_block
                         break
+            self.ct[i] = block
             self.pt += pt_block
             print self.pt.encode('hex')
             print self.pt
-            sleep(10)
+
+        # last block
+        i = len(self.ct) - 2
+        block = self.ct[i]
+        pt_block = ''
+        for j in range(1, 17):
+            #for guess in """ etoanihsrdluwmycgf,bp.kv'"I-T;_HAWMSB!j?ExLCDzPJNq:YOGRFU1XK*V()Q/023548697][$@Z#%&+<=>\^`{|}~""":
+            for guess in xrange(2, 256):
+                guess = chr(guess)
+                print pt_block.encode('hex')
+                if self._query_block(self._generate_block(guess, j, block), i):
+                    s = list(block)
+                    s[16 - j] = chr(ord(guess) ^ ord(s[16 - j]))
+                    block = ''.join(s)
+                    pt_block = guess + pt_block
+                    break
+        self.pt += pt_block
+        print self.pt.encode('hex')
+        print self.pt
+
 
 
 if __name__ == "__main__":
